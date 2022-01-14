f249fd04d9ff6d000ef4559078137707ddaba0c3
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-14 16:18:53 +0100

    create controllers and models

--- database/migrations/2022_01_14_143916_create_flights_table.php
+++ database/migrations/2022_01_14_143916_create_flights_table.php
@@ -16,6 +16,11 @@ class CreateFlightsTable extends Migration
         Schema::create('flights', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('location', 45);
+            $table->date('leave_date');
+            $table->date('return_date');
+            $table->string('package_name', 100);
+            $table->decimal('price', $precision = 8, $scale = 2);
         });
     }
 
