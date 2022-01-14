f476e41906c5c840a6500769647aca4fb9821dfe
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-14 17:59:06 +0100

    add more places

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts/layout')
 
 @section('content')
-<h3>Magnifica agenzia turistica Boolean presenta:
+<h3 class="text-center py-3">Magnifica agenzia turistica Boolean presenta:
 </h3>
 <div class="package_container d-flex flex-wrap">
     @foreach ($flights as $flight)
-        <div class="card mx-2">
+        <div class="card mx-2 p-3 text-center">
             <h2>{{$flight->location}}</h2>
             <p>Dal {{$flight->leave_date}} al {{$flight->return_date}}</p>
             <p>A soli {{$flight->price}} dobloni</p>
