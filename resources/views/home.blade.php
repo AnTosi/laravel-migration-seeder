d87ca586dacbacbec14c4e753aed4658a0da66fa
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-14 17:45:26 +0100

    create layout otherwise Fabio beats me up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,18 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts/layout')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    </head>
-    <body>
-        <h3>Magnifica agenzia turistica Boolean presenta:
-        </h3>
-        <div class="package_container d-flex flex-wrap">
-            @foreach ($flights as $flight)
-                <div class="card mx-2">
-                    <h2>{{$flight->location}}</h2>
-                    <p>Dal {{$flight->leave_date}} al {{$flight->return_date}}</p>
-                    <p>A soli {{$flight->price}} dobloni</p>
-                    <p>{{$flight->package_name}}!!!</p>
-                </div>
-                
-            @endforeach
-
+@section('content')
+<h3>Magnifica agenzia turistica Boolean presenta:
+</h3>
+<div class="package_container d-flex flex-wrap">
+    @foreach ($flights as $flight)
+        <div class="card mx-2">
+            <h2>{{$flight->location}}</h2>
+            <p>Dal {{$flight->leave_date}} al {{$flight->return_date}}</p>
+            <p>A soli {{$flight->price}} dobloni</p>
+            <p>{{$flight->package_name}}!!!</p>
         </div>
-    </body>
-</html>
+        
+    @endforeach
+
+</div>
+@endsection
