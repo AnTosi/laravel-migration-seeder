6c43db6cd02f845a20d56d91da8b61e15bff85d2
AnTosi <andrea.tosi.uni@gmail.com>
2022-01-17 15:42:24 +0100

    add single page flight route,  controller not yet there

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'FlightController@index');
 
 Route::get('/other', 'PageController@other');
+
+Route::get('flights/{flight}', 'FlightController@show')->name('flight');
